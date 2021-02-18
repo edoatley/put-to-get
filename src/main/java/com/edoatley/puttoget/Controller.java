3effeb8d74de0bab5c583790a6ac37774ab42a45
edoatley <edoatley@gmail.com>
2021-02-18 22:11:56 +0000

    trying one object approach

--- src/main/java/com/edoatley/puttoget/Controller.java
+++ src/main/java/com/edoatley/puttoget/Controller.java
@@ -1,22 +1,37 @@
 package com.edoatley.puttoget;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.Size;
+import javax.validation.Valid;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Map;
 
 @RestController
 @Validated
 public class Controller {
 
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    Validator validator = factory.getValidator();
+
     @GetMapping("/hello")
-    public String message(@RequestParam @Size(max=7) String name) {
-           return "Hello from GET " + name;
+    public String message(@RequestParam Map<String,String> allRequestParams) {
+           RequestBody body = new ObjectMapper().convertValue(allRequestParams, RequestBody.class);
+           validator.validate(body);
+           return "Hello from GET " + body.getName();
     }
 
     @PutMapping("/hello")
-    public String messageFromPut(@org.springframework.web.bind.annotation.RequestBody RequestBody body) {
+    public String messageFromPut(@Valid @org.springframework.web.bind.annotation.RequestBody RequestBody body) {
         String name = body.getName();
-        return message(name);
+        Map<String, String> map = new ObjectMapper().convertValue(body, new TypeReference<>() {});
+        return message(map);
     }
 }
--- src/main/java/com/edoatley/puttoget/RequestBody.java
+++ src/main/java/com/edoatley/puttoget/RequestBody.java
@@ -3,10 +3,16 @@ package com.edoatley.puttoget;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Validated
 public class RequestBody {
+
+    @Size(max=7)
     private String name;
 }
